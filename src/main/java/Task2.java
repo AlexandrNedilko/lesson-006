fa48d4dd45e1b17ff1ec9e14dde3c42ad8b5cd40
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-06-08 10:46:58 +0300

    Add 1-7 task

---
+++ src/main/java/Task2.java
@@ -0,0 +1,15 @@
+/*"2. Fix the method with try-catch to get the correct result of division:
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(divideByZero());
+    }
+
+    public static int divideByZero() {
+        int a = 5;
+        int b = 0;
+        return a / b;
+    }
+}"*/
+public class Task2 {
+}
---
+++ src/main/java/Task5.java
@@ -0,0 +1,15 @@
+/*"5. Throw a NullPointerException from the method iCanThrowException();
+        Handle it in the main method.
+public class Main {
+
+    public static void main(String[] args) {
+        iCanThrowException();
+    }
+
+    public static void iCanThrowException() {
+
+    }
+
+}"*/
+public class Task5 {
+}
